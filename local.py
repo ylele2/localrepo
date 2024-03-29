ffbe716b934733d549f5b7d4fbfe02c7a00dab4f
ylele2 <yasheshlele@hotmail.com>
2024-03-29 23:44:42 +0530

    deletel last line

--- local.py
+++ local.py
@@ -1,3 +1,2 @@
 print("Created in local folder and not directly in github")
 print("this line is added during new branch of dev (adding button)")
-print("this line is added during new branch of dev (radio button added to uat)")
